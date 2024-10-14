9eca71649584f64a2cd1696a89a9e88f99e89981
laqfresa <laqfresa@gmail.com>
2024-10-14 13:20:52 -0500

    Delete lq-frontend directory

--- lq-frontend/src/environments/environment.local.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  apiUrlsLQ: {
-    lq_internal: ' https://lq-fresa-internal-f6lmcxzrea-uw.a.run.app/internal'
-  },
-  environmentName: 'local',
-  production: false,
-}
