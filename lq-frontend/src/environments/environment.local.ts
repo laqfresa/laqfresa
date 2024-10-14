afe22c6d3b81faf35820bfffa64d991f6ea50e22
laqfresa <laqfresa@gmail.com>
2024-10-14 13:04:35 -0500

    Add files via upload

---
+++ lq-frontend/src/environments/environment.local.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  apiUrlsLQ: {
+    lq_internal: ' https://lq-fresa-internal-f6lmcxzrea-uw.a.run.app/internal'
+  },
+  environmentName: 'local',
+  production: false,
+}
